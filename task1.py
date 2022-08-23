4abf519ab2781616195859e6917c6afcd1fa404e
bsnh25 <bayuseptyan43@gmail.com>
2022-08-23 20:53:54 +0700

    menambahkan task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+#module 4
+
+## menggunakan module "if"
+luaslahan = float(input('masukkan luas lahan yang akan diolah :'))
+if luaslahan>=10:
+    print ("gunakan traktor roda 4")
+
+if luaslahan<=10:
+    print ("gunakan traktor roda 2")
+
+print ("penggunaan traktor ini direkomendasikan")
