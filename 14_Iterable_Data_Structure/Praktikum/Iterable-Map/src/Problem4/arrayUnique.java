57869ddaa892cbdb118ee8c4b8240c93b43576f1
bsnh25 <bayuseptyan43@gmail.com>
2022-09-20 17:20:36 +0700

    menambahkan file 13, 14, 15

--- 14_Iterable_Data_Structure/Praktikum/Iterable-Map/src/Problem4/arrayUnique.java
+++ 14_Iterable_Data_Structure/Praktikum/Iterable-Map/src/Problem4/arrayUnique.java
@@ -4,24 +4,33 @@ import java.util.*;
 
 public class arrayUnique {
     public static void main(String[] args) {
-        List<Integer> angka1 = new ArrayList<>(Arrays.asList(1,2,3,4));
-        List<Integer> angka2 = new ArrayList<>(Arrays.asList(1,2,5,10,16));
+        List<Integer> angka1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> angka2 = new ArrayList<>(Arrays.asList(1, 2, 5, 10, 16));
         List<Integer> arr = new ArrayList<>();
         arr.addAll(angka1);
         arr.addAll(angka2);
         System.out.println(arr);
-        for (int i = 0; i<arr.size();i++){
-            angka1.get(i)== angka2.get(i);
-            System.out.println(i);
-        }
 //        Set<Integer> set = new LinkedHashSet<>(angka1);
 //        int a1 = angka1.get();
 //        int a2 = angka2.get();
-//        int a = 0;
+        int a = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            a = arr.get(i);
+            for (int j = 0; j < arr.size(); j++) {
+                if (a == arr.get(j)) {
+                    if (i != j) {
+                        System.out.println("single Number = " + a);
+//                        System.out.println("Match found for "+a);
+                    }
+                }
+            }
+
 //        for (int i = 0; i<angka2.size(); i++)
-//        if (angka1.contains(angka2.)){
+//        if (arr.contains(angka2)){
+//            arr.remove(i)
 //            System.out.println("ada");
 //        }
 //        System.out.println(set);
         }
     }
+}
